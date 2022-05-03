2e2ddf47378bed11367d532d340716c9792690dc
Abi_Moreno <abigail.morenocruz98@gmail.com>
2022-05-03 11:14:16 -0500

    ejercicios con DOM

---
+++ JavaScript/DOM/assets/js/ejercicio2.js
@@ -0,0 +1,16 @@
+function getFormvalue(){
+    
+    const form=document.getElementById("form1");
+    
+    
+    const datos = Object.fromEntries(
+        new FormData(form)
+    )
+    
+    console.log(datos);
+    
+    
+    debugger;
+
+
+}
