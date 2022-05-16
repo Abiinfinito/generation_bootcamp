15af5a30647b88946c9894750f9a8388e2eb13a5
Abi_Moreno <abigail.morenocruz98@gmail.com>
2022-05-16 13:54:11 -0500

    ciclos JAVA

---
+++ java/JavaBasico/src/ciclos/CicloWhile.java
@@ -0,0 +1,25 @@
+package ciclos;
+
+import javax.swing.JOptionPane;
+
+public class CicloWhile {
+
+	public static void main(String[] args) {
+		String clave = "Pedro";
+		String comparacion = "";
+		
+		while (clave.equals(comparacion)== false) {
+			comparacion = JOptionPane.showInputDialog("Introduce la contraseña, por favor");
+			
+			if(clave.equals(comparacion)== false) {
+				System.out.println("La contraseña es incorrecta");
+			}
+		}
+		System.out.println("La contraseña es correcta");
+
+		
+		
+		
+	}
+
+}
---
+++ java/JavaBasico/src/ciclos/EjercicioNumero.java
@@ -0,0 +1,25 @@
+package ciclos;
+import javax.swing.JOptionPane;
+
+public class EjercicioNumero {
+	
+	    public static void main(String[] args) {
+
+	        int adivinar = (int) (Math.random()*(10-1));
+	        boolean salir = true;
+	        System.out.println(adivinar);
+
+	        while (salir){
+	            int input= Integer.valueOf(JOptionPane.showInputDialog("Introduce un numero"));
+	            if(input == adivinar){
+	                salir = false;
+	            }
+	            else{
+	                System.out.println("Numero incorrecto");
+	            }
+	        }
+	        System.out.println("Felicidades adivinaste el numero!!");
+	    }
+	}
+
+
