a38b15351b548c0908e528bf524f6b602c7aca45
Abi_Moreno <abigail.morenocruz98@gmail.com>
2022-05-17 12:06:21 -0500

    api JAVA

---
+++ java/POO/src/com/api/PruebaApi.java
@@ -0,0 +1,34 @@
+package com.api;
+
+import java.util.Scanner;
+
+
+public class PruebaApi {
+
+	public static void main(String[] args) {
+		
+		String nombre = "Abi";
+		System.out.println(nombre);
+		
+		String apellido = new String("Moreno");
+		System.out.println(apellido);
+		
+		
+		System.out.println(nombre.charAt(2));
+		
+		Integer.parseInt("50");
+		
+		
+		//Scanner
+		Scanner sc = new Scanner(System.in);
+		sc.nextInt();
+		sc.close();
+		
+		//Math
+		Math.random();
+		
+		
+		
+	}
+
+}
